7623c6e8c2c26ff59f248b16448334f812e3d74a
canh ps <canhphps04230@fpt.edu.vn>
2017-05-30 12:18:54 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/devshop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/devshop/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp64/www/devshop/catalog/');
+define('DIR_SYSTEM', 'C:/wamp64/www/devshop/system/');
+define('DIR_IMAGE', 'C:/wamp64/www/devshop/image/');
+define('DIR_LANGUAGE', 'C:/wamp64/www/devshop/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp64/www/devshop/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp64/www/devshop/system/config/');
+define('DIR_CACHE', 'C:/wamp64/www/devshop/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp64/www/devshop/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp64/www/devshop/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp64/www/devshop/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp64/www/devshop/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'devshop');
+define('DB_PASSWORD', '12345');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
